71d94da6c3633686771762e0ad1355ce628ddd72
abdulh70 <abdulazizh70@gmail.com>
2024-10-09 06:03:41 +0300

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+const num1 = 31;
+const num2 = 2;
+const num3 = 20;
+const num4 = 8;
+const multiply = 31 * 2;
+const random = Math.round(Math.random() * 10 + 1);
+const mod = num3 % num4;
+const max = Math.max(1, 2, 3, 4, 20);
